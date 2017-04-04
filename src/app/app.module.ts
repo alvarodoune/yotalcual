0c6d0d6c22b3bd354facd7c5d9da5df939630083
Alvaro Doune <alvarodoune@gmail.com>
2017-04-03 21:35:35 -0300

    add bootstrap and router.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,61 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
-import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
+import {AppComponent} from './app.component';
+import {LoginComponent} from './login/login.component';
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
+import {AlertModule} from 'ng2-bootstrap';
+
+
+const appRoutes: Routes = [
+      {path: '', component: LoginComponent},
+      {path: 'login', component: LoginComponent},
+      // {path: 'home', component: HomeComponent},
+      // {path: 'detail', component: DeliveryDetailComponent},
+      // {
+      //       path: 'delivery',
+      //       children: [
+      //             {
+      //                   path: "",
+      //                   component: DeliverysComponent
+      //             },
+      //             {
+      //                   path: ':id',
+      //                   component: DeliverysComponent
+      //             }
+      //       ]
+      // },
+      // {path: 'delete', component: DeleteDeliveryComponent},
+      {
+            path: '',
+            redirectTo: '/login',
+            pathMatch: 'full'
+      },
+      {
+            path: '**',
+            component: PageNotFoundComponent
+      }
+];
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+      declarations: [
+            AppComponent,
+            LoginComponent,
+            PageNotFoundComponent
+      ],
+      imports: [
+            BrowserModule,
+            FormsModule,
+            HttpModule,
+            RouterModule.forRoot(appRoutes),
+            AlertModule.forRoot()
+            // AdministrationModule
+      ],
+      providers: [],
+      bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
