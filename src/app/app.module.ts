fd7eee177c2ae2a520e2b4507281a107041fb546
Alvaro Doune <alvarodoune@gmail.com>
2017-04-11 23:23:48 -0300

    login and pageNotFound modules.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,52 +1,17 @@
 import {BrowserModule} from "@angular/platform-browser";
 import {NgModule} from "@angular/core";
-import {RouterModule, Routes} from "@angular/router";
+import {RouterModule} from "@angular/router";
 import {FormsModule} from "@angular/forms";
 import {HttpModule} from "@angular/http";
 
 import {AppComponent} from "./app.component";
-import {LoginComponent} from "./login/login.component";
-import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
 import {AlertModule} from "ng2-bootstrap";
 import {CoreModule} from "./core/core.module";
 import {ShellComponent} from "./core/shell/shell.component";
 
-
-const appRoutes: Routes = [
-      {path: '', component: LoginComponent},
-      {path: 'login', component: LoginComponent},
-      // {path: 'home', component: HomeComponent},
-      // {path: 'detail', component: DeliveryDetailComponent},
-      // {
-      //       path: 'delivery',
-      //       children: [
-      //             {
-      //                   path: "",
-      //                   component: DeliverysComponent
-      //             },
-      //             {
-      //                   path: ':id',
-      //                   component: DeliverysComponent
-      //             }
-      //       ]
-      // },
-      // {path: 'delete', component: DeleteDeliveryComponent},
-      {
-            path: '',
-            redirectTo: '/login',
-            pathMatch: 'full'
-      },
-      {
-            path: '**',
-            component: PageNotFoundComponent
-      }
-];
-
 @NgModule({
       declarations: [
-            AppComponent,
-            LoginComponent,
-            PageNotFoundComponent
+            AppComponent
       ],
       imports: [
             BrowserModule,
@@ -55,7 +20,6 @@ const appRoutes: Routes = [
             CoreModule,
             RouterModule.forRoot(appRoutes),
             AlertModule.forRoot()
-            // AdministrationModule
       ],
       providers: [],
       bootstrap: [ShellComponent]
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,47 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {RouterModule, Routes} from "@angular/router";
+
 import {ShellComponent} from './shell/shell.component';
-import { TopBarComponent } from './shell/top-bar/top-bar.component';
-import { MainContentComponent } from './shell/main-content/main-content.component';
+import {TopBarComponent} from './shell/top-bar/top-bar.component';
+import {MainContentComponent} from './shell/main-content/main-content.component';
+import {LoginComponent} from "../login/login.component";
+import {PageNotFoundComponent} from "../page-not-found/page-not-found.component";
+
+const appRoutes: Routes = [
+      {path: '', component: LoginComponent},
+      {path: 'login', component: LoginComponent},
+      // {path: 'home', component: HomeComponent},
+      // {path: 'detail', component: DeliveryDetailComponent},
+      // {
+      //       path: 'delivery',
+      //       children: [
+      //             {
+      //                   path: "",
+      //                   component: DeliverysComponent
+      //             },
+      //             {
+      //                   path: ':id',
+      //                   component: DeliverysComponent
+      //             }
+      //       ]
+      // },
+      // {path: 'delete', component: DeleteDeliveryComponent},
+      {
+            path: '',
+            redirectTo: '/login',
+            pathMatch: 'full'
+      },
+      {
+            path: '**',
+            component: PageNotFoundComponent
+      }
+];
 
 @NgModule({
       imports: [
-            CommonModule
+            CommonModule,
+            RouterModule
       ],
       declarations: [ShellComponent, TopBarComponent, MainContentComponent],
       exports: [ShellComponent]
